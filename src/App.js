eef0b02afc85c394364e404985de89e63f31c88c
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-07 03:32:22 +0600

    simple form build and event hadler used

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import app from "./firebase-init";
 const auth = getAuth(app);
 function App() {
-  const hadleEmailChange = e =>{ //ekta parameter er jonno na dile o cholto 
+  const hadleEmailBlur = e =>{ //ekta parameter er jonno na dile o cholto 
     console.log(e.target.value)
+  }
+  const hadlePasswordBlur = e =>{ //ekta parameter er jonno na dile o cholto 
+    console.log(e.target.value)
+  }
+
+  const handleFormSubmit = e =>{
+    console.log("submitted")
+    e.preventDefault();
   }
   return (
     <div className="App">
-       <form action="#">
-           <input onChange={hadleEmailChange} type="email" />
-           <input type="password" name="" id="" />
+       <form onSubmit={handleFormSubmit}>
+           <input onBlur={hadleEmailBlur} type="email" />
+           <input onBlur={hadlePasswordBlur} type="password" name="" id="" />
+           <br />
+           <input type="submit"  vlaue="login" name="" id="" /> 
        </form>
     </div>
   );
